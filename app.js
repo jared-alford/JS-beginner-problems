89202fbeb2e4ab0fb0382f319f8f2ab925625a8f
Jared Alford <alford.jared@gmail.com>
2024-12-15 05:31:57 -0800

    notes on JS beginner challenges

---
+++ app.js
@@ -0,0 +1,126 @@
+/*
+function addition(num1, num2) {
+  return num1 + num2
+}
+
+console.log(addition(8, 5));
+
+function hoursIntoSeconds(hours) {
+  return hours * 3600
+}
+
+console.log(hoursIntoSeconds(4));
+
+function calcPerimeter(length, width) {
+  return length * 2 + width * 2
+}
+
+console.log(calcPerimeter(2, 2));
+
+function calcAreaOfTriangle(base, height) {
+  return (base * height) / 2;
+}
+
+console.log(calcAreaOfTriangle(5, 8));
+
+function appendFrontend(string) {
+  return string + " Frontend";
+}
+
+console.log(appendFrontend("banana"));
+
+
+function sumGreaterThan100(num1, num2) {
+  return num1 + num2 > 100
+}
+
+console.log(sumGreaterThan100(6, 3));
+
+function lessThanOrEqualToZero(num) {
+  return num <= 0;
+}
+
+console.log(lessThanOrEqualToZero(-8));
+
+
+function oppositeBoolean(bool) {
+  if (bool === true)
+    return false;
+  else if (bool === false) 
+    return true;
+}
+
+function oppositeBoolean(bool) {
+  return !bool;
+}
+console.log(oppositeBoolean(false))
+
+
+
+function isNotZero(num) {
+  if (num !== 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+  
+  function isNotZero(num) {
+    return num !== 0;
+  }
+
+console.log(isNotZero(-42));
+
+
+function calcRemainder(num1, num2) {
+  return num1 % num2;
+}
+
+console.log(calcRemainder(73, 3));
+
+
+function isOdd(num) {
+  if (num % 2 === 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function isOdd(num) {
+  return num % 2 === 0;
+}
+
+console.log(isOdd(21));
+
+
+function booleanInteger(num) {
+  if (num % 2 === 0) {
+    return  1;
+  }
+  else {
+    return -1;
+  }
+}
+
+function booleanInteger(num) {
+  return num % 2 === 0 ? 1 : -1;
+}
+
+console.log(booleanInteger(3));
+
+
+
+function isLoggedInAndSubscribed(string1, string2) {
+  if (string1 === "LOGGED_IN" && string2 === "SUBSCRIBED") {
+    return true;
+  }
+  return false;
+}
+
+function isLoggedInAndSubscribed(string1, string2) {
+  return (string1 === 'LOGGED_IN') || (string2 === 'SUBSCRIBED');
+  )
+}
+*/
